3bf509ebc8525bb3357454f6fe4d56c0ca957170
henrick.nogueira <ricknogueira1231@gmail.com>
2025-08-05 14:39:26 -0300

    4.6. Implementando a camada de domínio para Delivery

---
+++ Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/Delivery.java
@@ -0,0 +1,37 @@
+package com.algaworks.algadelivery.delivery.tracking.domain.model;
+
+import lombok.EqualsAndHashCode;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Delivery {
+
+    @EqualsAndHashCode.Include
+    private UUID id;
+    private UUID courierId;
+
+    private DeliveryStatus status;
+
+    private OffsetDateTime placedAt;
+    private OffsetDateTime assignedAt;
+    private OffsetDateTime expectedDeliveryAt;
+    private OffsetDateTime fulfilledAt;
+
+    private BigDecimal distanceFee;
+    private BigDecimal courierPayout;
+    private BigDecimal totalCost;
+
+    private Integer totalItems;
+
+    private ContactPoint sender;
+    private ContactPoint recipient;
+
+    private List<Item> items = new ArrayList<>();
+
+
+}
