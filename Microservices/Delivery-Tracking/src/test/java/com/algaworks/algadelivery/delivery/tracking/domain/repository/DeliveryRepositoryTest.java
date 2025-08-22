1d2338708fd460b2e14735ed4ec51ba4a440a37f
henrick.nogueira <ricknogueira1231@gmail.com>
2025-08-22 15:21:38 -0300

    5.5. Testes de integração para persistência

---
+++ Microservices/Delivery-Tracking/src/test/java/com/algaworks/algadelivery/delivery/tracking/domain/repository/DeliveryRepositoryTest.java
@@ -0,0 +1,66 @@
+package com.algaworks.algadelivery.delivery.tracking.domain.repository;
+
+import com.algaworks.algadelivery.delivery.tracking.domain.model.ContactPoint;
+import com.algaworks.algadelivery.delivery.tracking.domain.model.Delivery;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class DeliveryRepositoryTest {
+
+    private DeliveryRepository deliveryRepository;
+
+    @Test
+    public void shouldPersist() {
+        Delivery delivery = Delivery.draft();
+
+        delivery.editPreparationDetails(createdValidPreparationDetails());
+
+        delivery.addItem("Computador", 2);
+        delivery.addItem("Notebook", 2);
+
+        deliveryRepository.saveAndFlush(delivery);
+
+        Delivery persistedDelivery = deliveryRepository.findById(delivery.getId()).orElseThrow();
+        assertEquals(2, persistedDelivery.getItems().size());
+    }
+
+    private Delivery.PreparationDetails createdValidPreparationDetails() {
+        ContactPoint sender = ContactPoint.builder()
+                .zipCode("00000-000")
+                .street("Rua São Paulo")
+                .number("100")
+                .complement("Sala 401")
+                .name("João Silva")
+                .phone("(11) 90000-1234")
+                .build();
+
+
+        ContactPoint recipient = ContactPoint.builder()
+                .zipCode("12331-342")
+                .street("Rua Brasil")
+                .number("500")
+                .complement("")
+                .name("Maria Silva")
+                .phone("(11) 91345-1332")
+                .build();
+
+
+
+        return Delivery.PreparationDetails.builder()
+                .sender(sender)
+                .recipient(recipient)
+                .distanceFee(new BigDecimal("15.00"))
+                .courierPayout(new BigDecimal("5.00"))
+                .expectedDeliveryTime(Duration.ofHours(5))
+                .build();
+    }
+
+}
